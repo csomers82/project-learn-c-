7dc147cb3a99163157593a1581b24f363c160ffe
Clifton Somers <csomers@purdue.edu>
2016-05-10 17:46:49 -0400

    interesting bug in memory management

--- project-learn-c++/trivialock.cpp
+++ project-learn-c++/trivialock.cpp
@@ -16,6 +16,11 @@
 
 using namespace std;
 
+
+
+
+
+
 /////////////////////////////////////////////////////////////////
 ///
 //	linked list template or C++
@@ -28,13 +33,14 @@ class Node {
 		//constructors
 		Node();
 		Node(const T& item, Node<T>* nxt=NULL);
+		//~Node();//destructor
 		//node content
 		T data;
 		//methods
-		Node<T> * getnxt();
-		void insert(Node<T>* target);
-		void destroy(Node<T>* head);
-}
+		Node<T> *	getnxt();
+		void		setnxt(Node<T> * n);
+		void		insert(Node<T>* target);
+};
 
 
 template <class T>
@@ -44,49 +50,121 @@ Node<T>::Node()
 }
 
 template <class T>
-Node<T>::Node(const T& item, Node<T>* next=NULL)
+Node<T>::Node(const T& item, Node<T>* next)
 {
 	this->data = item;
 	this->next = next;
 }
 
 template <class T>
-Node<T>* Node::getnxt()
+Node<T>* Node<T>::getnxt()
 {
 	return this->next;
 }
 
 template <class T>
-void Node::insert(Node<T>* target)
+void Node<T>::setnxt(Node<T> * nxt)
+{
+	this->next = nxt;
+	cout << "woah\n";
+}
+
+template <class T>
+void Node<T>::insert(Node<T>* target)
 {
 	// save ptrs
-	Node<T> * after = target.getnxt();     
+	Node<T> * after = this->getnxt();     
 	Node<T> * follow = this;
 	Node<T> * end = NULL;
 
 	// reroute and evaluate
-	target.next = this;
+	this->setnxt(target);
 	if(after == NULL) {
+		//cout << "uh oh\n";
 		return;
 	}
 	// find the end of the inserted (list)
 	do {
-		end = follow.getnxt();
+		end = follow->getnxt();
 	} while(end != NULL);
 	// reattached the end we saved 
 	follow->next = after;
 }
 
-template <class T>
-void Node::destroy(Node<T>* target)
+/*template <class T>
+Node<T>::~Node()
 {
-	Node<T> * after = target.getnxt();     
-	if(after) {
-		after.destroy();
+	Node<T> * after = this->getnxt();     
+	Node<T> * at = this;
+	while(after != NULL) {
+		delete at;
+		at = after;
+		after = at->getnxt();
 	}
-	delete target->data;
-	delete target;
+}*/
+/////////////////////////////////////////////////////////////////
+///
+//	linked list template or C++
+//		abstracts the concept of a node into a generic template
+///
+template <class T>
+class List {
+		Node<T> *top;
+		Node<T>	*bot;
+	public:
+		// structors
+		List();
+		List(Node<T>*);
+		~List();
+		// methods
+		Node<T>*	pull(int);
+		int			count();
+		void		add(Node<T>* imp);
+		int			isempty();
+};
+
+template <class T>
+List<T>::List() { bot = NULL; top = NULL;  }
+
+template <class T>
+List<T>::List(Node<T> * first) {
+	this->top = first;
+	this->bot = first;
 }
+
+template <class T>
+List<T>::~List() { 
+	delete this->top;
+	//delete this;// this line is not needed why?
+}
+
+template <class T>
+Node<T> * List<T>::pull(int index) {
+	if(this->isempty) return NULL;
+	Node<T> * elem = this->top;
+
+	for(int jndex = 0; jndex < index; ++jndex)
+	{	
+		elem = elem->getnxt();
+	}
+	return elem;
+}
+
+template <class T>
+int List<T>::isempty() {
+	return (this->top == NULL) ? TRUE: FALSE;
+}
+
+template <class T>
+void List<T>::add(Node<T> * caboose) {
+	if(this->isempty() == TRUE) return;
+	cout << "checkpoint\n";
+	this->bot->insert(caboose);
+	this->bot = caboose;
+}
+
+
+
 
 /////////////////////////////////////////////////////////////////
 ///
@@ -95,8 +173,8 @@ void Node::destroy(Node<T>* target)
 //		as the statistics associated with current and past access attempts.
 ///
 class Vault {
-		List * answers;// string array of (encrypted) answers
-		List * questions;// string array of (encrypted) questions
+		List<string> * answers;// string array of (encrypted) answers
+		List<string> * questions;// string array of (encrypted) questions
 		unsigned int vaultcount;//number of entries in the vault
 		unsigned int past_accesses;// total previous accesses
 		string * failurelog;// a list of failure occurances
@@ -117,7 +195,7 @@ Vault::Vault (FILE* fp) {
 
 	//allocate buffer for entire file (assumed small)
 	fseek(fp, 0, SEEK_END);
-	fSize = ftell(buffer);
+	fSize = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
 	buffer = new char [fSize];
 	//read file and parse
@@ -128,7 +206,7 @@ Vault::Vault (FILE* fp) {
 		delete[] buffer;
 	}		
 	else {
-		target1 = buffer;
+	/*	target1 = buffer;
 		for(int index = 0; index < NSECTIONS; ++index) 
 		{
 			section = strtok(target, SDELIM);
@@ -138,6 +216,9 @@ Vault::Vault (FILE* fp) {
 			}
 			target2 = section;
 			for(int jndex = 0; 
+		}
+	*/
+	}
 }
 
 int Vault::attempt() {
@@ -156,10 +237,20 @@ int Vault::attempt() {
 
 
 int main(int argc, char * argv[]) {
-	FILE * fp = fopen(VAULTFILE, "r");
-	if(fp == NULL) return(EXIT_FAILURE);
-	Vault * vault = new Vault(fp);	
 	
-	delete vault;
-	fclose(fp);
+	string d1 = "honey";
+	string d2 = "dingus";
+	Node<string> * biscuit = new Node<string>(d1); 
+	//Node<string> * ralphi  = new Node<string>(d2); 
+	List<string> * litz = new List<string>(biscuit);	
+	//litz->add(ralphi);
+	//List<string> * litz = new List<string>();	
+	//FILE * fp = fopen(VAULTFILE, "r");
+	//if(fp == NULL) return(EXIT_FAILURE);
+	//Vault * vault = new Vault(fp);	
+	
+
+	delete litz;
+	//delete vault;
+	//fclose(fp);
 }
