5fa724406cba2a933fc89d77d89ec2aa875d4e24
Clifton Somers <csomers@purdue.edu>
2016-05-29 17:05:27 -0400

    started remodeling of explode function

--- project-learn-cpp/trivialock.cpp
+++ project-learn-cpp/trivialock.cpp
@@ -33,7 +33,7 @@ template <class T>
 class Node {
 		Node <T> *next;//the content of the node
 	public:
-		//constructors
+		//conStructors
 		Node();
 		Node(const T& item, Node<T>* nxt=NULL);
 		//~Node();//destructor
@@ -49,7 +49,7 @@ class Node {
 template <class T>
 Node<T>::Node()
 {
-	//empty constructor for vanilla allocation
+	//empty conStructor for vanilla allocation
 }
 
 template <class T>
@@ -116,11 +116,11 @@ class List {
 		int			isempty();
 };
 
-/// empty initialization: constructs list w/o nodes
+/// empty initialization: conStructs list w/o nodes
 template <class T>
 List<T>::List() { bot = NULL; top = NULL;  }
 
-// constructs list with one (1) node
+// conStructs list with one (1) node
 template <class T>
 List<T>::List(Node<T> * first) {
 	this->top = first;
@@ -222,7 +222,7 @@ class Vault {
 Vault::Vault (FILE* fp) {
 	//initialization variables
 	char * buffer			= NULL;
-	long fSize				= 0;
+	unsigned long fSize		= 0;
 	size_t result			= 0;
 	int index				= 0;
 	char * line				= NULL;// a question, answer, or statistic
@@ -245,7 +245,7 @@ Vault::Vault (FILE* fp) {
 	else {
 		target1 = buffer;
 		// outter loop :: grab a section from the buffer
-		section = std::strtok(target1, SDELIM);
+		section = strsep(&target1, SDELIM);
 		while(section != NULL) 
 		{
 			cout << "3\n";
@@ -258,8 +258,8 @@ Vault::Vault (FILE* fp) {
 				//cout << "line = _" << line << "_\n";
 				if(line != NULL)
 				lstobj->addnew(line);
-				target2 = NULL;
-				line = std::strtok(target2, LDELIM);
+				//target2 = NULL;
+				line = strsep(&target2, LDELIM);
 			}
 			cout << "1\n";
 			switch(index++) {
@@ -271,7 +271,8 @@ Vault::Vault (FILE* fp) {
 						break;
 			}
 			cout << "2\n";
-			target1 = NULL;
+			
+			//target1 = NULL;
 			section = std::strtok(target1, SDELIM);
 		}			
 		cout << "testfile = " << result << " bytes.\n";
@@ -294,6 +295,141 @@ int Vault::attempt() {
 
 
 
+/////////////////////////////////////////////////////////////////
+///
+//	explode:	takes a string and delimiting chars and splits
+//				the first into an array of strings w/o preserving
+//				the delimeters. The resulting string is in a single 
+//				allocated block
+//		
+//		params:		const string line - input to be parsed
+//					const string delim - delimiting chars
+//					int * nSec - number of delinations
+//		returns		char ** fragments - subsection array
+///
+char ** explode(const string line, const string delimeters, int& nSec) {
+	//LOCAL DECLARATIONS
+	unsigned int di;// delimiter index
+	unsigned int ci;// character index in line
+	unsigned int dc;// delimeter char count
+	unsigned int cc;// line char count
+	int strx = 0;// expected number of strings
+	int nStr = 0;// number of strings created
+	int flag = 0;// denotes whether the indexed char is a delimeter
+	int prev = 1;// is prev of flag - starts as 1 to ensure 1 char does not generate a str
+	int tcount = 0;
+	char * lense;// the reference to copy of line
+	char * save;// ptr to first char of copy of line
+	char * delims;// a c char * of delimiting chars
+	char ** fragments = NULL;// return array of delinations 
+
+	//EXECUTABLE STATEMENTS
+	delims = (char *) delimeters.c_str();
+	save = (char *) line.c_str();
+	lense = save;
+	cc = line.length();
+	dc = delimeters.length();
+	//1) determine # of expected strings
+	for(ci = 0; ci < cc; ++ci) {
+		flag = 0;
+		for(di = 0; ((di < dc) && (flag == 0)); ++di) {
+			if(lense[ci] == delims[di]) {
+				//cout << "char d = " << delims[di] << endl;
+				//cout << "char c [" << ci << "] = " << lense[ci] << endl;
+				flag = 1;
+				//cout << "flag = " << flag << endl;
+			}
+			//cout << "flag = " << flag << endl;
+		}
+		if(prev == 0 && flag == 1) { // end of string of non delimeters
+			++strx;
+			cout << "*" << endl;
+		}
+		prev = flag;
+		//cout << endl;
+	}
+	if(flag == 0) {
+		++strx;
+	}
+	
+	//2) allocate the data for each pointer
+	fragments = new char * [strx];// char* for every expected string
+	flag = 0;
+	ci = 0;
+	//3) find the first nondelimeter char in copy of line
+	while((*lense != '\0') && (strchr(delims, lense[ci]) != NULL)) {
+		flag = 0;
+		for(di = 0; ((di < dc) && (flag == 0)); ++di) {
+			if(lense[ci] == delims[di]) {
+				flag = 1;
+			}
+		}
+		++lense;
+		ci += 1;
+	}
+	cout << "\nlense = \"" << *lense << "\"\n";
+	/*
+	//4) iterate through and find complete strings
+	if(*lense != '\0') {
+		fragments[0] = lense;
+		nStr = 1;
+		tcount = 0;
+
+		while(nStr < dc + 1 && *lense != '\0') {
+			if(*lense != '\0') {
+				// {if last char was part of delimiter}
+				if(flag == 1) {
+					// {if curr char is not a delimiter}
+					if(strchr(delims, *lense) == NULL) {
+						// {if not last fragment: assign part and reset}
+						if(nStr < dc + 1) {
+							fragments[nStr++] = lense;
+							flag = 0;
+							tcount = 0;
+						}
+					}
+				}// {check for delimiter at curr char: sever is found}
+				else if(strchr(delims, *lense) != NULL) {
+					flag = 1;
+					fragments[nStr - 1][tcount] = '\0';
+				}
+				// {update curr char: update char in fragment}
+				lense++;
+				tcount++;
+			}
+		}
+	}	
+	*/
+	return(fragments);
+	
+	nSec = strx;	
+}
+
+void test_explode() {
+	cout << "\n\nexplode" << endl;
+	char ** frags = NULL;
+	const string s1 = string("\n\rvedi	veni viki\vand.\fitwasgood\n\n");
+	//string("taco... beanstaco.pork... ... ...cheese.");
+	const string f1 = string("\t\v\n\f\r ");
+	int length = 0;
+
+	frags = explode(s1, f1, length);
+	cout << "string1: (" << s1 << ")\n";
+	cout << "delim1: (" << f1 << ")\n";
+	cout << "length = " << length << endl;
+	/*if(length) {
+		for(int p = 0; p < length; ++p) {
+			cout << "\nfrags[" << p << "] = \"" << frags[p] << "\"";
+		}
+	}*/
+	delete [] frags;
+}
+
+
+/////////////////////////////////////////////////////////////////
+///
+//	main:
+///
 int main(int argc, char * argv[]) {
 	
 	string d1 = "honey";
@@ -307,18 +443,21 @@ int main(int argc, char * argv[]) {
 	//List<string> * litz = new List<string>();	
 	FILE * fp = fopen(VAULTFILE, "r");
 	if(fp == NULL) return(EXIT_FAILURE);
-	Vault * vault = new Vault(fp);	
+	//Vault * vault = new Vault(fp);	
 	
-	cout << "Qs\n";
-	vault->questions->print();
-	cout << "As\n";
-	vault->answers->print();
-	cout << "Ss\n";
-	vault->statistics->print();
+	//cout << "Qs\n";
+	//vault->questions->print();
+	//cout << "As\n";
+	//vault->answers->print();
+	//cout << "Ss\n";
+	//vault->statistics->print();
 	
 	delete litz;
-	delete vault;
+	//delete vault;
 	fclose(fp);
+
+	test_explode();
+
 }
 
 
